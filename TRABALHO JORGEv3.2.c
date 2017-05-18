2c793b318af7804f003067f8ecc59793b07130f2
pedrorosarioo <pedro.ufba2012@hotmail.com>
2017-05-18 02:23:01 -0300

    Update TRABALHO JORGEv3.2.c

--- TRABALHO JORGEv3.2.c
+++ TRABALHO JORGEv3.2.c
@@ -420,13 +420,16 @@ char nova[100], *r;
             j++;
         }
     }
-    if (strchr(v, nova[j-1])==NULL){ //GARANTE QUE A FUNÇÃO ENCERRE COM UMA CONSTANTE PARA NÃO CRASHAR O WHILE DO MAIN
-        nova[j]='*';
-        j++;
-        nova[j]='1';
-        j++;
-    }
     nova[j]='\0';
+    if (strcmp(nova, "FIM")!=0){ //SE FOR "FIM" NÃO MODIFIQUE
+        if (strchr(v, nova[j-1])==NULL){ //GARANTE QUE A FUNÇÃO ENCERRE COM UMA CONSTANTE PARA NÃO CRASHAR O WHILE DO MAIN
+            nova[j]='*';
+            j++;
+            nova[j]='1';
+            j++;
+            nova[j]='\0';
+        }
+    }
     strcpy(c, nova);
 //    printf("%s", c);
 }
