0a5d413a139c1da3f3cc4296f0767059cb34bb8b
pedrorosarioo <pedro.ufba2012@hotmail.com>
2017-07-11 23:53:04 -0300

    Update Polinomios2.c

--- Polinomios2.c
+++ Polinomios2.c
@@ -28,7 +28,7 @@ NoLista *literais; // Aponta para uma fila com os literais
 int sinal;
 }expressao, *pExpressao;
 
-// ------------------------- FUNÇÕES PARA PREENCHER AS ESTRUTURAS ------------------------------------------
+// ------------------------- FUNÃ‡Ã•ES PARA PREENCHER AS ESTRUTURAS ------------------------------------------
 
 void zerastring(char c[]){ //PREENCHE A STRING COM \0 PARA EVITAR LIXO DE MEMORIA
 int i;
@@ -53,13 +53,11 @@ int i;
     while(aux!=NULL){
     aux2=aux->lprox;
         while ((aux2)&&(aux->variavel==aux2->variavel)){
-//            printf("entrei\n");
             aux->expoente = aux->expoente+aux2->expoente;
             aux->lprox=aux2->lprox;
             if (aux2->lprox!=NULL) (aux2->lprox)->lant=aux;
             free(aux2);
             aux2=aux->lprox;
-//            printf("%d\n", aux->expoente);
         }
         aux=aux->lprox;
     }
@@ -96,7 +94,6 @@ if (aux){
         aux=aux->prox;
     }
     if (aux->ant==NULL){
-//        printf("Entrei\n");
         *p=aux->prox;
         if(aux->prox) (*p)->ant=NULL;
     }else if (aux->prox==NULL){
@@ -119,7 +116,6 @@ if (dep==NULL){
     *x=n;
     n->lprox=NULL;
     n->lant=NULL;
- //   printf("entrei1\n");
 }else{
     while (dep!=NULL){
         if (n->variavel<dep->variavel)break;
@@ -130,17 +126,15 @@ if (dep==NULL){
         n->lprox=*x;
         n->lant=NULL;
         *x=n;
-//        printf("entrei2\n");
     }else{
         n->lprox=ant->lprox;
         ant->lprox=n;
         n->lant=ant;
-//        printf("entrei3\n");
     }
 }
 }
 
-pExpressao criacelulas(int n){//cria o espaço para comportar a expressão
+pExpressao criacelulas(int n){//cria o espaÃ§o para comportar a expressÃ£o
 int i, cont=0;
 pExpressao r, origem, aux, aux2;
     for(i=0; i<n; i++){
@@ -158,10 +152,7 @@ pExpressao r, origem, aux, aux2;
             aux->sinal=0;
             aux->indice=0;
             aux->denominador=1;
-//            zerastring(aux->indicec);
             zerastring(aux->polinomio);
-            //printf("%s\n", aux->polinomio);
-  //          origem->indice=0;
         }else{
             aux2=(pExpressao)malloc(sizeof(expressao));
             if(aux2==NULL){
@@ -170,19 +161,15 @@ pExpressao r, origem, aux, aux2;
             }
             cont++;
             zerastring(aux2->polinomio);
-//            printf("Entrou, %d \n", cont);
             aux->prox=aux2;
             aux2->prox=NULL;
             aux2->ant=aux;
             aux2->literais=NULL;
             aux2->sinal=0;
             aux2->denominador=1;
-//            aux2->indice=0;
             aux=aux2;
-    //        aux2->indice=1;
         }
     }
-//    printf("%d celulas criadas\n", cont);
     r=origem;
     return r;
 }
@@ -209,27 +196,22 @@ Lista aux=*p, novoNo;
     for(i=0; c[i]!='\0'; ){
         if (c[i]!='^'){
             if (strchr(v, c[i])==NULL){
- //               printf("entrei\n");
                 novoNo=(Lista)malloc(sizeof(NoLista));
                 novoNo->variavel=c[i];
-                //printf("%c\n", novoNo->variavel);
                 i=i+2;
                 if(c[i]=='-'){
                   flag2=1;
                   i++;
                 }
-//                printf("%c\n", c[i]);
                 pontchar=strchr(v, c[i]);
                 if (pontchar){
-                while((pontchar)&&(c[i]!='\0')){     //"x^32y^23c^3defblaus^51"
-                    auxiliar[j]=c[i];
-//                    printf("%c ", auxiliar[j]);
-                    j++;
-                    i++;
-                    pontchar=strchr(v, c[i]);
+                    while((pontchar)&&(c[i]!='\0')){     //"x^32y^23c^3defblaus^51"
+                        auxiliar[j]=c[i];
+                        j++;
+                        i++;
+                        pontchar=strchr(v, c[i]);
+                    }
                 }
-                }
-//                printf("%s\n", auxiliar);
                 if (flag2) novoNo->expoente=criaindice(auxiliar, j) * -1;
                 else novoNo->expoente=criaindice(auxiliar, j);
                 if (novoNo->expoente!=0) InsereLista(p, novoNo);
@@ -256,12 +238,10 @@ if(x<0) x=x*-1;
 return cont;
 }
 
-void numpravet(int n, int *v){ // PASSA CADA ALGARISMO DE UM NUMERO PARA UM ESPAÇO DO VETOR
+void numpravet(int n, int *v){ // PASSA CADA ALGARISMO DE UM NUMERO PARA UM ESPAÃ‡O DO VETOR
 int i, k, cont=0;
     for(i=contalgarismos(n)-1; i>=0; i--){
         v[i]=n%10;
-//        printf("%d ", v[i]);
-//        printf("%d ", v[i]);
         n=n/10;
     }
 }
@@ -269,7 +249,6 @@ int i, k, cont=0;
 void numprachar(int n, char c[]){
 int v[100], k, i=0;
 numpravet(n, v);
-//printf("%d\n", v[0]);
   for(k=0; k<contalgarismos(n); k++){
         c[i]=v[k]+'0';
         i++;
@@ -282,89 +261,67 @@ int i=0, k=0, j=0, v[100], z, flag;
 char m[1000]="\0";
 Lista LIST;
 pExpressao EXP = *p;
-for(LIST=(*p)->literais;LIST!=NULL;LIST=LIST->lprox){
-    flag=1;
-//    printf("%d\n", LIST->expoente);
-    m[i]=LIST->variavel;
-//    printf("%d", LIST->expoente);
-//    printf("%c", m[i]);
-    i++;
-    m[i]='^';
-//    printf("%c", m[i]);
-    i++;
-    if(LIST->expoente<0){
-      flag=0;
-      z=LIST->expoente * -1;
-//      printf("z= %d\n", z);
-      m[i]='-';
-      i++;
-    }
-//    printf("%d\n", LIST->expoente);
-    if (flag) numpravet(LIST->expoente, v);
-    else numpravet(z, v);
-    for(k=0; k<contalgarismos(LIST->expoente); k++){
-        m[i]=v[k]+'0';
-//        printf("m[i] = %c\n", m[i]);
-//        printf("%d ", v[k]);
-        //m[i]=intochar(v[k]);
- //       printf("%c", m[i]);
+    for(LIST=(*p)->literais;LIST!=NULL;LIST=LIST->lprox){
+        flag=1;
+        m[i]=LIST->variavel;
         i++;
-//        printf("ate agora: %s\n", m);
-    }
+        m[i]='^';
+        i++;
+        if(LIST->expoente<0){
+            flag=0;
+            z=LIST->expoente * -1;
+            m[i]='-';
+            i++;
+        }
+        if (flag) numpravet(LIST->expoente, v);
+        else numpravet(z, v);
+        for(k=0; k<contalgarismos(LIST->expoente); k++){
+            m[i]=v[k]+'0';
+            i++;
+        }
     strcpy(EXP->polinomio, m);
+    }
 }
-//printf("%s", m);
-}
 
 
 
 
-void entrada(char c[], pExpressao *x){ // COLOCA INDICE E POLINOMIO NOS ESPAÇOS ALOCADOS
+void entrada(char c[], pExpressao *x){ // COLOCA INDICE E POLINOMIO NOS ESPAÃ‡OS ALOCADOS
 int i, j=0, k=0, l=0, flag=1;
 char v[11], in[11]="\0";
 strcpy(v, "0123456789");
 pExpressao forfree, aux=*x;
     for (i=0; ((i<TAM_POLINOMIO)&&(c[i]!='\0')); i++){
-//        zerastring(aux->polinomio);
         flag=1;
         if (!aux) break;
         if ((c[i]=='-')&&(aux->sinal==0)&&(k==0))/*caso haja "-1-1"*/{
           aux->sinal=1;
           i++;
         }
-//        printf("%d\n",aux->indice);
         if ((strchr(v, c[i]))&&(flag)){
-                //printf("%c\n", c[i]);
                 in[k]=c[i];
                 k++;
                 l++;
         }else{
-            //printf("%c\n", c[i]);
             if (flag){
                 aux->indice = criaindice(in, k);
                 if (aux->sinal==1) aux->indice= (aux->indice)*(-1);
-                //printf("%d\n", aux->indice);
                 flag=0;
                 k=0;
-//                printf("%c\n", c[i]);
             }
-            //if (c[i])
-            //printf("%c\n", c[i]);
             while((c[i]!='+')&&(c[i]!='*')&&(c[i]!='-')&&(c[i]!='\0')){ // COPIA TUDO ATE ACHAR UM OPERADOR
                 aux->polinomio[j]=c[i];
-                if ((c[i]!='^')&&(strchr(v, c[i])==NULL)&&(c[i+1]!='^')){//COLOCA ^1 NOS QUE ESTÃO SEM EXPOENTE
+                if ((c[i]!='^')&&(strchr(v, c[i])==NULL)&&(c[i+1]!='^')){//COLOCA ^1 NOS QUE ESTÃƒO SEM EXPOENTE
                     aux->polinomio[j+1]='^';
                     aux->polinomio[j+2]='1';
                     j=j+2;
-               }//else{*/
+               }
                 i++;
                 j++;
-                //}
             }
             j=0;
             aux->operador=c[i];
             l=0;
-//            printf("Finalizei\n");
             if ((aux->ant)&&((aux->ant)->operador=='-')){
                     (aux->ant)->operador='+';
                     aux->indice=(aux->indice)*(-1);
@@ -466,7 +423,6 @@ int flag=0;
       aux2 = aux->prox;
       if (aux->indice==0){
         if (aux==*p) flag=1;
-//        aux->prox=NULL;
         if (aux->ant) (aux->ant)->operador = aux->operador;
         excluimonomio(p, aux);
       }
@@ -480,7 +436,7 @@ int flag=0;
 }
 
 
-// ---------------------- FUNÇÕES PARA OPERAR AS ESTRUTURAS ------------------------------
+// ---------------------- FUNÃ‡Ã•ES PARA OPERAR AS ESTRUTURAS ------------------------------
 void multiplica (pExpressao *p, pExpressao *q, pExpressao r){
 Lista aux=r->literais, aux2;
     (*q)->indice= (*q)->indice * r->indice;
@@ -492,23 +448,18 @@ Lista aux=r->literais, aux2;
         aux=aux2;
     }
     r->literais=NULL;
-//    excluimonomio(p, r);
-//    atualiza(p);
 }
 
 void multiplica2 (pExpressao *p, pExpressao *q, pExpressao r){//
 Lista aux=r->literais, aux2;
     (*q)->indice= (*q)->indice * r->indice;
-//    (*q)->operador=r->operador;
     while (aux!=NULL){
         aux2=aux->lprox;
         aux->lprox=NULL;
         InsereLista(&((*q)->literais), aux);
         aux=aux2;
-//        r->literais=aux2;
     }
     r->literais=NULL;
-//    excluimonomio(p, r);
     atualiza(p);
 }
 
@@ -520,8 +471,6 @@ Lista aux=r->literais;
         else if ((*q)->operador == '-') (*q)->indice = (*q)->indice + r->indice;
         if(r->operador!='\0') (*q)->operador=r->operador;
         else (r->ant)->operador=r->operador;
-//        excluimonomio(p, r);
-//        atualiza(p);
         return 1;
     }
 }
@@ -533,7 +482,6 @@ while ((aux)&&(aux->prox != NULL)){
     while (aux->operador=='*'){
         multiplica(p, &aux, aux->prox);
         excluimonomio(p, aux->prox);
-//        printf("%s\n", aux->polinomio);
     }
     aux=aux->prox;
 }
@@ -543,7 +491,6 @@ aux=*p;
         while (aux2!=NULL){
             v=aux2->prox;
             if(soma(p, &aux, aux2)) excluimonomio(p, aux2);
-//            if (strcmp(aux->polinomio, aux2->polinomio)==0) excluimonomio(p, aux2);
             aux2=v;
         }
         aux=aux->prox;
@@ -562,20 +509,17 @@ char nova[100], *r;
         }
     }
     nova[j]='\0';
-    if (strcmp(nova, "FIM")!=0){ //SE FOR "FIM" NÃO MODIFIQUE
-//        if (strchr(v, nova[j-1])==NULL){ //GARANTE QUE A FUNÇÃO ENCERRE COM UMA CONSTANTE PARA NÃO CRASHAR O WHILE DO MAIN
+    if (strcmp(nova, "FIM")!=0){
             nova[j]='*';
             j=j+1;
             nova[j]='1';
             j=j+1;
             nova[j]='\0';
- //       }
     }
     strcpy(c, nova);
-//    printf("%s", c);
 }
 
-int verificad(pExpressao p){ //VERIFICA SE O POLINOMIO É COMPOSTO APENAS PELA CONSTANTE 1
+int verificad(pExpressao p){ //VERIFICA SE O POLINOMIO Ã‰ COMPOSTO APENAS PELA CONSTANTE 1
   if(p){
     if ((p->indice==1)&&(p->prox==NULL)&&(p->ant==NULL)&&(strcmp(p->polinomio, "\0")==0)) return 0;
     return 1;
@@ -592,7 +536,7 @@ pExpressao aux;
 }
 
 // --------------------------------------------------------------------------------------
-// ----------------------------------------------- OPERAÇÕES ENTRE POLINOMIOS -----------
+// ----------------------------------------------- OPERAÃ‡Ã•ES ENTRE POLINOMIOS -----------
 
 pExpressao copia(pExpressao p){
 int cont=0;
@@ -634,16 +578,12 @@ void som(pExpressao *p, pExpressao *q){
   aux->operador='+';
   aux->prox=*q;
   *q=NULL;
-  //termosemelhante(p);
   atualiza(p);
-  //printaexpressao(*p);
-  //printf("\n");
 }
 
 void mul(pExpressao *p, pExpressao *q){ //multiplica 2 polinomios
 pExpressao aux, aux2, v, auxiliar, k, cp1, cp2, z;
 int i=0, flag=1;
-//auxiliar=copia(*p);
   for(v=*q; v!=NULL; v=v->prox){
     for (cp1=copia(*p), aux=cp1; aux!=NULL; aux=aux->prox){
         z=copia(*q);
@@ -657,7 +597,6 @@ int i=0, flag=1;
     }else{
         k->prox=cp1;
         k->operador='+';
-//        termosemelhante(&cp2);
         atualiza(&cp2);
     }
     i++;
@@ -801,7 +740,7 @@ char s[TAM_POLINOMIO];
       menu_altera_Acumulador(p, i);
 
     }else if(OP=='2'){
-      printf("Digite a expressao que você deseja somar: ");
+      printf("Digite a expressao que vocÃª deseja somar: ");
       scanf(" %[^\n]s", s);
       fflush(stdin);
       cria(aux, s);
@@ -809,7 +748,7 @@ char s[TAM_POLINOMIO];
       refresh(p);
       menu_altera_Acumulador(p, i);
     }else if(OP=='3'){
-      printf("Digite a expressao que você deseja subtrair: ");
+      printf("Digite a expressao que vocÃª deseja subtrair: ");
       scanf(" %[^\n]s", s);
       fflush(stdin);
       cria(aux, s);
@@ -818,7 +757,7 @@ char s[TAM_POLINOMIO];
       menu_altera_Acumulador(p, i);
 
     }else if(OP=='4'){
-      printf("Digite a expressao que você deseja multiplicar: ");
+      printf("Digite a expressao que vocÃª deseja multiplicar: ");
       scanf(" %[^\n]s", s);
       fflush(stdin);
       cria(aux, s);
@@ -827,7 +766,7 @@ char s[TAM_POLINOMIO];
       menu_altera_Acumulador(p, i);
 
     }else if(OP=='5'){
-      printf("Digite a expressao que você deseja dividir: ");
+      printf("Digite a expressao que vocÃª deseja dividir: ");
       scanf(" %[^\n]s", s);
       fflush(stdin);
       cria(aux, s);
